97dcca4fa31a3598fd8a84196865ea9a4f577a78
VikasEdger <vikaskumargt41@gmail.com>
2024-09-21 23:05:54 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -20,17 +20,18 @@ resource "aws_instance" "kubernetes_server" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update -y",
-      "sudo apt-get install docker.io -y",
-      "wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+      "sudo apt-get install -y docker.io curl",
       "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
       "sudo chmod +x minikube-linux-amd64",
-      "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
-      "curl -LO https://storage.googleapis.com/kubernetes-release/release/stable.txt",
+      "sudo mv minikube-linux-amd64 /usr/local/bin/minikube",
+      "curl -LO \"https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl\"",
       "sudo chmod +x kubectl",
-      "sudo cp kubectl /usr/local/bin/kubectl",
-      "sudo groupadd docker",
+      "sudo mv kubectl /usr/local/bin/kubectl",
+      "sudo groupadd docker || true",
       "sudo usermod -aG docker ubuntu",
-      "echo 'wait to start the instance'"
+      "echo 'Instance setup complete'"
     ]
   }
 
