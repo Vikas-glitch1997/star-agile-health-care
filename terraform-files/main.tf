3117cff0b74600257ee6fa5479a3d69699fcc271
VikasEdger <vikaskumargt41@gmail.com>
2024-09-22 10:42:55 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "kubernetes_server" {
-  ami                    = "ami-0e86e20dae9224db8"
-  instance_type         = "t2.medium"
+  ami           = "ami-0e86e20dae9224db8"
+  instance_type = "t2.medium"
   vpc_security_group_ids = ["sg-042e7c08102a1660a"]
-  key_name              = "virginia"
+  key_name = "virginia"
 
   root_block_device {
     volume_size = 20
@@ -18,31 +18,25 @@ resource "aws_instance" "kubernetes_server" {
   }
 
   provisioner "remote-exec" {
-  inline = [
-    "sudo apt-get update -y",
-    "sudo apt-get install -y curl",
-    "curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl",
-    "sudo chmod +x kubectl",
-    "sudo mv kubectl /usr/local/bin/",
-    "sudo apt-get install -y docker.io",
-    "sudo systemctl start docker",
-    "sudo groupadd docker || true",
-    "sudo usermod -aG docker ubuntu"
-  ]
-}
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install docker.io -y",
+      "wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+      "sudo systemctl start docker",
+      "sudo chmod +x minikube-linux-amd64",
+      "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
+      "curl -LO https://storage.googleapis.com/kubernetes-release/release/stable.txt",
+      "sudo chmod +x kubectl",
+      "sudo cp kubectl /usr/local/bin/kubectl",
+      "sudo groupadd docker",
+      "sudo usermod -aG docker ubuntu"
+    ]
+  }
 
   connection {
     type        = "ssh"
     host        = self.public_ip
     user        = "ubuntu"
-    private_key = file("./virginia.pem")
-  }
-
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} > inventory"
-  }
-
-  provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/Medicure_healthcare_Project/terraform-files/ansibleplaybook.yml"
+    private_key = file("./project.pem")
   }
 }
