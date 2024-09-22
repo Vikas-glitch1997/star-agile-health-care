1512c54d1cd55653bd6aa8ecf00af8c4e62cd87a
VikasEdger <vikaskumargt41@gmail.com>
2024-09-22 15:19:00 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "kubernetes_server" {
-  ami           = "ami-0e86e20dae9224db8"
-  instance_type = "t2.medium"
+  ami                    = "ami-0e86e20dae9224db8"
+  instance_type          = "t2.medium"
   vpc_security_group_ids = ["sg-042e7c08102a1660a"]
-  key_name = "virginia"
+  key_name               = "virginia"
 
   root_block_device {
     volume_size = 20
@@ -25,21 +25,27 @@ resource "aws_instance" "kubernetes_server" {
       "sudo systemctl start docker",
       "sudo chmod +x minikube-linux-amd64",
       "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
-      
-      # Download kubectl binary
       "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
       "sudo chmod +x kubectl",
       "sudo cp kubectl /usr/local/bin/kubectl",
-      
-      "sudo groupadd docker || true",  # Group might already exist
-      "sudo usermod -aG docker ubuntu"
+      "sudo groupadd docker || true",
+      "sudo usermod -aG docker ubuntu",
+      "echo 'Instance setup complete.'"
     ]
-}
+  }
 
   connection {
     type        = "ssh"
     host        = self.public_ip
     user        = "ubuntu"
     private_key = file("./virginia.pem")
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.kubernetes_server.public_ip} > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/Medicure_healthcare_Project/terraform-files/ansibleplaybook.yml"
   }
 }
