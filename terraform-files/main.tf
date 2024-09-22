96151c3a76108aaef302ba13292319373149e6a9
VikasEdger <vikaskumargt41@gmail.com>
2024-09-22 21:24:51 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -18,21 +18,23 @@ resource "aws_instance" "kubernetes_server" {
   }
 
   provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update -y",
-      "sudo apt-get install docker.io -y",
-      "wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
-      "sudo systemctl start docker",
-      "sudo chmod +x minikube-linux-amd64",
-      "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
-      "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
-      "sudo chmod +x kubectl",
-      "sudo cp kubectl /usr/local/bin/kubectl",
-      "sudo groupadd docker || true",
-      "sudo usermod -aG docker ubuntu",
-      "echo 'Instance setup complete.'"
-    ]
-  }
+  inline = [
+    "sudo apt-get update -y",
+    "sudo apt-get install docker.io -y",
+    "sudo systemctl start docker",
+    "wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+    "sudo chmod +x minikube-linux-amd64",
+    "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
+    "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
+    "sudo chmod +x kubectl",
+    "sudo cp kubectl /usr/local/bin/kubectl",
+    "sudo groupadd docker || true",
+    "sudo usermod -aG docker ubuntu",
+    "sudo systemctl restart docker",
+    "minikube start --driver=docker",
+    "echo 'Instance setup complete.'"
+  ]
+}
 
   connection {
     type        = "ssh"
